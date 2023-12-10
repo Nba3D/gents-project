8ce055bd38d4330c7ca58b1f82d17d930ccf6cc5
rh-fran6 <francis.anyaegbu@francisanyaegbu.local>
2023-12-09 19:10:13 -0600

    commit of baseline code

---
+++ z.js
@@ -0,0 +1,19 @@
+
+
+let myArray = ["obi", "sinachi", "ugo", "daddy"]
+
+// myArray.push(8)
+
+// myArray[8] = 200
+
+myArray.push("lion")
+
+console.log(myArray)
+
+myArray.pop()
+
+console.log(myArray)
+
+for ( let i = 0; i <= myArray.length - 1;  i++ ) {
+    console.log(myArray[i])
+}
